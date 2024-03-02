a4a640fa2a73cb84ae3eb542b4a6ed2456b9d571
M. Uzair Zulfiqar <uzairmian856@gmail.com>
2024-03-02 14:52:30 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,37 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CrouselComponent } from './crousel/crousel.component';
+import { BrandsComponent } from './brands/brands.component';
+import { ProductsComponent } from './products/products.component';
+import { PaymenttypeComponent } from './paymenttype/paymenttype.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CrouselComponent,
+    BrandsComponent,
+    ProductsComponent,
+    PaymenttypeComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/paymenttype/paymenttype.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paymenttype',
+  templateUrl: './paymenttype.component.html',
+  styleUrl: './paymenttype.component.css'
+})
+export class PaymenttypeComponent {
+
+}
